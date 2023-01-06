89b0253af2edd64fe8b2bbeb50629c5f4b281548
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2023-01-07 06:28:57 +0900

    fix lambda

--- cicd/lambda_handler.py
+++ cicd/lambda_handler.py
@@ -5,5 +5,5 @@ def lambda_handler(event, context):
     print(event)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello Pipeline!')
+        'body': json.dumps('Hello Lambda!')
     }
